f7f7f58f0220326a7602dfc6459c446c7ad48383
Asaf Ohayon <asaf@sysbind.co.il>
2021-04-13 21:16:54 +0300

    web components poc

---
+++ src/Main.elm
@@ -0,0 +1,35 @@
+module Main exposing (..)
+
+import Html exposing (text, Html)
+import Browser
+
+type Msg
+    = NoOp
+
+
+type alias Model =
+    { name : String
+    }
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    ( model, Cmd.none )
+
+view : Model -> Html Msg
+view model =
+  text ("Hello world, my name is: " ++ model.name)
+
+
+type alias Flags =
+    { name : String }
+
+init : Flags -> ( Model, Cmd Msg )
+init flags =
+    ( Model flags.name, Cmd.none )
+
+
+main : Program Flags Model Msg
+main =
+    Browser.element
+        { init = init, update = update, subscriptions = \_ -> Sub.none, view = view }
